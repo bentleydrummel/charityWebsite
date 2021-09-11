c19cc5d62f3d2f79b447dc7af09d2c3e4bcd18cc
Izzy Ferrandez <issy.ferrandez@gmail.com>
2021-09-11 22:58:45 +0100

    My first commit

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+function navHome() {
+  window.location.href = "file:///C:/Users/isabe/OneDrive/COLLEGE%20PREP/Computer%20science/charityWebsite/index.html#home";
+}
+
+function navGallery() {
+  window.location.href = "file:///C:/Users/isabe/OneDrive/COLLEGE%20PREP/Computer%20science/charityWebsite/index.html#gallery";
+}
